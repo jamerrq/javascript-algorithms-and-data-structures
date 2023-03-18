2a1863ae45375f2ada54492e2e16f8acc7166c18
Jamer Rebolledo <jamerrq@gmail.com>
2023-03-18 01:18:15 -0500

    reached 50% of Basic JavaScript module

---
+++ declare-string-variables.js
@@ -0,0 +1,2 @@
+var myFirstName = "Jamer";
+var myLastName = "Rebolledo";
---
+++ shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [
+  ["Banana", 2],
+  ["Apple", 3],
+  ["Coconout", 1],
+  ["Bread", 2],
+  ["Avocado", 3]
+];
