227f7e4a8cf85acc84427f9a36e6f199983d73d6
Jamer Rebolledo <jamerrq@gmail.com>
2023-03-25 11:20:14 -0500

    added Basic Data Structures module | via Alekhine

---
+++ Basic Data Structures/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,9 @@
+let myNestedArray = [
+  // Only change code below this line
+  "level one",
+  ["level two"],
+  [["deep"]],
+  [[["deeper"]]],
+  [[[["deepest"]]]],
+  // Only change code above this line
+];
---
+++ Basic Data Structures/use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,1 @@
+let yourArray = [false, "uno", 2, 3, 4]; // Change this line
